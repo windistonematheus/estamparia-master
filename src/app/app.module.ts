4cd6fca38a72ec17d8743bf37a0fca4bc61ccc95
Diego Maylan <diego_maylan@hotmail.com>
2018-08-16 22:20:06 -0300

    Inserir componentes da estrutura basica do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { ProductImageComponent } from './components/product-image/product-image.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    SidenavComponent,
+    ProductImageComponent
   ],
   imports: [
     BrowserModule
