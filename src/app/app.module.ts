0cd01de2a55832ba9dd4621420b9d0e5f748f585
DiegoMaylan <diego_maylan@hotmail.com>
2018-09-02 20:53:42 -0300

    Adicionar angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 
-import { SidenavComponent } from './components/sidenav/sidenav.component';
-import { ProductImageComponent } from './components/product-image/product-image.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ServiceComponent } from './components/service/service.component';
+import { BudgetComponent } from './components/budget/budget.component';
+import { LoginComponent } from './components/login/login.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductsComponent } from './components/products/products.component';
+import { MugsComponent } from './components/mugs/mugs.component';
+import { TShirtsComponent } from './components/t-shirts/t-shirts.component';
+import { BabyLooksComponent } from './components/baby-looks/baby-looks.component';
+import { BabyLookViewComponent } from './components/baby-look-view/baby-look-view.component';
+import { TShirtViewComponent } from './components/t-shirt-view/t-shirt-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     NavbarComponent,
-    SidenavComponent,
-    ProductImageComponent
+    FooterComponent,
+    HomeComponent,
+    ServiceComponent,
+    BudgetComponent,
+    LoginComponent,
+    CartComponent,
+    ProductsComponent,
+    MugsComponent,
+    TShirtsComponent,
+    BabyLooksComponent,
+    BabyLookViewComponent,
+    TShirtViewComponent
   ],
   imports: [
     BrowserModule
