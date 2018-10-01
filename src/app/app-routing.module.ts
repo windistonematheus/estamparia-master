ec90ec5a4f7bc690954e83a197905d4dce6df7f6
windistonematheus <windistonematheus@gmail.com>
2018-10-01 18:37:33 -0300

    Tela Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { ContactComponent } from './components/contact/contact.component';
 import { BabyLookComponent } from './components/baby-look/baby-look.component';
 import { BudgetComponent } from './components/budget/budget.component';
 import { ProductComponent } from './components/product/product.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'baby-look', component: BabyLookComponent },
   { path: 'budget', component: BudgetComponent },
-  { path: 'produto', component:ProductComponent}
+  { path: 'produto', component:ProductComponent},
+  { path: 'login', component:LoginComponent}
 ];
 
 @NgModule({
