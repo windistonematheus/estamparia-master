cc378fe0d477d71fb8b8e6de982406f5e9e29184
Viviannie <viviannie@gmail.com>
2018-11-25 20:30:23 -0300

    Contato criado

---
+++ UI/src/app/models/Contatos.ts
@@ -0,0 +1,8 @@
+export class Contato {
+    id: number;
+    nome: string;
+    email: string;
+    telefone: string;
+    assunto: string;
+    mensagem: string;
+}
--- api/src/routes.php
+++ api/src/routes.php
@@ -66,3 +66,47 @@ $app->get('/camisetas', function ($request, $response, $args) {
    $produto = $sth->fetchAll();
    return $this->response->withJson($produto);
 });
+
+$app->get('/contatos', function ($request, $response, $args) {
+    $sth = $this->db->prepare("SELECT * FROM contato");
+   $sth->execute();
+   $contato = $sth->fetchAll();
+   return $this->response->withJson($contato);
+});
+
+$app->post('/contato', function ($request, $response) {
+    $input = $request->getParsedBody();
+    $sql = "INSERT INTO contato (nome, email, telefone, assunto, mensagem) VALUES (:nome, :email, :telefone, :assunto, :mensagem)";
+     $sth = $this->db->prepare($sql);
+    $sth->bindParam("nome", $input['nome']);
+    $sth->bindParam("email", $input['email']);
+    $sth->bindParam("telefone", $input['telefone']);
+    $sth->bindParam("assunto", $input['assunto']);
+    $sth->bindParam("mensagem", $input['mensagem']);    
+    $sth->execute();
+    $input['id'] = $this->db->lastInsertId();
+    return $this->response->withJson($input);
+});
+
+$app->delete('/contato/[{id}]', function ($request, $response, $args) {
+    $sth = $this->db->prepare("DELETE FROM contato WHERE id=:id");
+   $sth->bindParam("id", $args['id']);
+   $sth->execute();
+   $contatos = $sth->fetchAll();
+   return $this->response->withJson($contatos);
+});
+
+$app->put('/contato/[{id}]', function ($request, $response, $args) {
+    $input = $request->getParsedBody();
+    $sql = "UPDATE contato SET nome=:nome, email=:email, telefone=:telefone, assunto=:assunto, mensagem=:mensagem WHERE id=:id";
+     $sth = $this->db->prepare($sql);
+    $sth->bindParam("id", $args['id']);
+    $sth->bindParam("nome", $input['nome']);
+    $sth->bindParam("email", $input['email']);
+    $sth->bindParam("telefone", $input['telefone']);
+    $sth->bindParam("assunto", $input['assunto']);
+    $sth->bindParam("mensagem", $input['mensagem']);
+    $sth->execute();
+    $input['id'] = $args['id'];
+    return $this->response->withJson($input);
+});
