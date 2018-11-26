91144b71fcf992d555d38fdfd34061bc128e78fb
Viviannie <viviannie@gmail.com>
2018-11-25 21:33:31 -0300

    Cadastro - models / registro - components

---
+++ UI/src/app/models/Cadastros.ts
@@ -0,0 +1,14 @@
+export class Cadastros {
+    id: number;
+    nome: string;
+    email: string;
+    cpf: string;
+    senha: string;
+    cep: string;
+    logradouro: string;
+    numero: string;
+    complemento: string;
+    bairro: string;
+    cidade: string;
+    telefone: string;
+}
